25840d75acc2360fce75be9b5391838198e10b27
Matthis Raveau <raveau@insa-toulouse.fr>
2022-10-28 14:32:07 +0200

    ADC fonctionnel avec software trigger on passe à la suite

--- Drivers/adc.c
+++ Drivers/adc.c
@@ -3,26 +3,26 @@
 
 int valeur_ADC;
 
-void ADC_init_single_continuous(MyADC_Struct_TypeDef * adc){
-	adc->ADC->CR2 &= ~ADC_CR2_CONT;
+void ADC_init_single(MyADC_Struct_TypeDef * adc){
+	RCC->CFGR |= RCC_CFGR_ADCPRE_DIV6; //clock 12MHz
+	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN; 
+  adc->ADC->CR2 &= ~ADC_CR2_CONT;
 	adc->ADC->CR2 |= ADC_CR2_EXTTRIG;
+	adc->ADC->CR1 |= ADC_CR1_EOCIE;
 	NVIC_SetPriority(ADC1_2_IRQn,adc->prio);
 	NVIC_EnableIRQ(ADC1_2_IRQn);               
 	//pour set les timers qui peuvent déclencher la converion
-	//adc->ADC->CR2 |= (ADC_CR2_EXTSEL & ~ADC_CR2_EXTSEL_1);
-	//TIM4_CC4 event : 101 a la page 225 RM008
-	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN; 
-  RCC->CFGR |= RCC_CFGR_ADCPRE_DIV6; //clock 12MHz
+	adc->ADC->CR2 |= ADC_CR2_EXTSEL ;
+	//la on est en software control bit
 	adc->ADC->SQR1&= ADC_SQR1_L; //nombre de conversion = 1 seule voie 
   adc->ADC->SQR3|= adc -> voie; // on choisit la seule voie a converir 
+	adc->ADC->CR2|= ADC_CR2_ADON;
 }
 
 void ADC_on (MyADC_Struct_TypeDef * adc){
-		if (adc->ADC->CR2 & ADC_CR2_ADON){
 				adc->ADC->CR2|= ADC_CR2_ADON;
-		}
-	//si jamais on est déja on on ne lance pas la conversion en voulant allumer l'ADC
 }
+
 
 	void ADC_off (MyADC_Struct_TypeDef * adc){
 		 adc->ADC->CR2 &= ~ADC_CR2_ADON;
@@ -30,7 +30,6 @@ void ADC_on (MyADC_Struct_TypeDef * adc){
 
 	int ADC_Read(MyADC_Struct_TypeDef * adc){	
 		//interruption ou EOC est set ; on peut read 
-		//while (!(adc->ADC->SR & ADC_SR_EOC)){}
     return adc->ADC->DR & ~((0x0F) << 12); //les 12 bits qui nous intéressent
 	}
 	
@@ -39,3 +38,4 @@ void ADC_on (MyADC_Struct_TypeDef * adc){
 		//ADC2->SR &= ~ADC_SR_EOC;
 		valeur_ADC = ADC1->DR & ~((0x0F) << 12);
 	}
+
--- Drivers/adc.h
+++ Drivers/adc.h
@@ -16,29 +16,8 @@ typedef struct
 //eventuellement créer un trype case pour chaque mode et faire un seul init 
 		//avec un grand if
 
-	void ADC_init_single_continuous(MyADC_Struct_TypeDef * adc);
-	//CONT BIT ? 
-	//EOC FLAG ? 
-	/*The AWD analog watchdog status bit is set if the analog voltage converted by the ADC is 
-			below a low threshold or above a high threshold. These thresholds are programmed in the 
-			12 least significant bits of the ADC_HTR and ADC_LTR 16-bit registers. An interrupt can be 
-			enabled by using the AWDIE bit in the ADC_CR1 register.
-			The threshold value is independent of the alignment selected by the ALIGN bit in the 
-			ADC_CR2 register. The comparison is done before the alignment (see Section 11.5).
-			The analog watchdog can be enabled on one or more channels by configuring the 
-			ADC_CR1 register as shown in Table 66.*/
-	//SCAN BIT 
-	/*Scan mode can be selected by setting the SCAN bit in the ADC_CR1 register. Once this bit 
-			is set, ADC scans all the channels selected in the ADC_SQRx registers (for regular 
-			channels) or in the ADC_JSQR (for injected channels). A single conversion is performed for 
-			each channel of the group. After each end of conversion the next channel of the group is 
-			converted automatically. If the CONT bit is set, conversion does not stop at the last selected 
-			group channel but continues again from the first selected group channel.
-			When using scan mode, DMA bit must be set and the direct memory access controller is 
-			used to transfer the converted data of regular group channels to SRAM after each update of 
-			the ADC_DR register.
-			The injected channel converted data is always stored in the ADC_JDRx registers.*/
-
+	void ADC_init_single(MyADC_Struct_TypeDef * adc);
+	
 	void ADC_on (MyADC_Struct_TypeDef * adc);
 /*The ADC can be powered-on by setting the ADON bit in the ADC_CR2 register. When the
 	ADON bit is set for the first time, it wakes up the ADC from Power Down mode.*/
@@ -53,6 +32,8 @@ typedef struct
 			Il faut donc modifier cette fréquence en configurant les registres du RCC. 
 			Pour cela, ajoutez la ligne suivante qui va permettre de diviser par 6 la 
 			fréquence d'entrée (72 MHz) */
-	
+
+#define MyADC_SWSTART(A)(A->CR2|=ADC_CR2_SWSTART) 
+//Macro pour le software trigger
 
 #endif
