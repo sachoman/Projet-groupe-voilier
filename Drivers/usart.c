3996bafb2cacccc4d40bc38aa04afc36e2ba98f9
sachoman <sachadarthenucq@gmail.com>
2022-10-28 14:34:08 +0200

    drivers usart terminés

--- Drivers/usart.c
+++ Drivers/usart.c
@@ -1,20 +1,54 @@
 #include "stm32f10x.h"
 #include "usart.h"
+#include "timer.h"
+#include "Driver_GPIO.h"
 
+
+static char Data;
+void (* usart_ptr) (void);
 //registre USART1 Rx= PA10
 //Usart1 TX = PA9
 
-void configure_usart1_9600bps(){
+MyGPIO_Struct_TypeDef Usart_gpio;
+MyTimer_Struct_TypeDef Usart_tim4;
+
+char usart_buffer[UsartBufferSize];
+int usart_buffer_index=0;
+
+void usart_configure_1_9600bps(){
     RCC->APB2ENR |= RCC_APB2ENR_USART1EN; // Validation horloge
     USART1->CR1 |= USART_CR1_UE; // Activation
+		USART1->CR1 |= USART_CR1_RE;
     USART1->CR1 &= ~USART_CR1_M; // Taille de 8 bits de données
     USART1->CR2 &= USART_CR2_STOP; // Choix d'un seul bit de stop
     USART1->BRR |= 468 << 4; // Mantisse register à 468 -> 9600 baud rate
     USART1->BRR |= 75; // Fraction register à 75 -> 9600 baud rate
     USART1->CR1 |= USART_CR1_TE; // Transmitter enable
+		Usart_gpio.GPIO= GPIOA;
+		Usart_gpio.GPIO_Conf=AltOut_Ppull;
+		Usart_gpio.GPIO_Pin=9;
+		MyGPIO_Init(&Usart_gpio);
 }
 
-void send(char data){
-    USART1->DR |= data; // Ecriture de la donnée dans le registre DR
-    while(!(USART1->SR & USART_SR_TC)) {} // Attente de la fin de transmission
+void usart_1_send(char data){
+    USART1->DR |= data; // Ecriture de la donnée dans DR
+    while(!(USART1->SR & USART_SR_TXE)) {} // Attente si data register empty
 }
+
+void USART1_IRQHandler(){
+	Data=USART1->DR;
+	usart_ptr();
+}
+
+char usart_getValue(void)
+{
+	return Data;
+}
+
+void usart_1_configure_IT(void (*fun) (void), char prio){
+	USART1->CR1 |= USART_CR1_RXNEIE;
+	NVIC_EnableIRQ(USART1_IRQn);
+	NVIC_SetPriority(USART1_IRQn, prio);
+	usart_ptr = fun;
+}
+
--- Drivers/usart.h
+++ Drivers/usart.h
@@ -2,8 +2,13 @@
 #define usarth
 #include "stm32f10x.h"
 
+#define UsartTimer TIM4
+#define UsartTimerArr 10000
+#define UsartTimerPsc 720
+#define UsartBufferSize 10
 
-void configure_usart1_9600bps();
-void send(char data);
-
+void usart_configure_1_9600bps();
+void usart_1_send(char data);
+void usart_1_configure_IT(void (*fun) (void), char prio);
+char usart_getValue(void);
 #endif
