1ec2bf6775666061793c6b13bc8809b1d9a33ebb
sachoman <sachadarthenucq@gmail.com>
2022-11-07 11:36:45 +0100

    fin rotation plateau

--- Services/Rotation-plateau/RTE/_Reel/RTE_Components.h
+++ Services/Rotation-plateau/RTE/_Reel/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'usart-dev' 
+ * Project: 'rotation-plateau' 
  * Target:  'Reel' 
  */
 
--- Services/Rotation-plateau/Sources/principal.c
+++
@@ -1,5 +0,0 @@
-#include "stm32f10x.h"
-#include "Driver_GPIO.h"
-
-int main(void){
-}
